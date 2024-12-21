c419752e8fe186bf063b06566b87d05837cceb99
marcosgatica2003 <marcosgatica1112@gmail.com>
2024-12-21 17:04:39 -0300

    Ejercicio rectángulo terminado

--- clasesYObjetos/rectangulo/rectangulo.java
+++ clasesYObjetos/rectangulo/rectangulo.java
@@ -1,7 +1,32 @@
-import javax 
+class figuraRectangulo {
+    private float altura;
+    private float ancho;
+
+    public figuraRectangulo (float _altura, float _ancho) { this.dimensionar(_altura, _ancho); }
+
+    public void dimensionar (float _altura, float _ancho) {
+        this.altura = _altura;
+        this.ancho = _ancho;
+    }
+
+    public void imprimirDatos () {
+        System.out.print("Altura: \t");
+        System.out.println(this.altura);
+        System.out.print("Ancho: \t");
+        System.out.println(this.ancho);
+
+    }
+
+    public float calcularArea () {
+       return this.altura * this.ancho;
+    }
+}
 
 public class rectangulo {
     public static void main(String argv[]) {
-        p
+        figuraRectangulo rec1 = new figuraRectangulo(0, 0);
+        rec1.dimensionar(30,20);
+        rec1.imprimirDatos();
+        System.out.println(rec1.calcularArea());
     }
 }
